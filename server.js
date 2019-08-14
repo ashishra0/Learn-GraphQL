1f0fa073db632f2e6d396b7c4d66f5e2bfd625f9
Ashish Rao <ashishrao2598@gmail.com>
2019-08-14 18:07:44 +0530

    add graphiql to local machine

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const {
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLObjectType
+} = require('graphql')
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Helloworld',
+    fields: () => ({
+      message: { 
+        type: GraphQLString,
+        resolve: () => 'Hello world'  
+      }
+    })
+  })
+})
+const app = express()
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql: true
+}))
+app.listen(5000, () => console.log("app is up and running"))
